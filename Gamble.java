65b1f915dee0efca58a6f147bef0c117cd5609eb
TristenLiu5 <electricemerald0825@gmail.com>
2026-05-07 21:53:06 +0000

    Yo Jun, I fixed the slot machine, we could make it a 9 grid slot machine if you feel like it

--- Gamble.java
+++ Gamble.java
@@ -13,32 +13,41 @@ public class Gamble
         
     }
     
-    public void options() {
+    public void options() 
+    {
+        boolean failsafe = false;
         
-        
-        System.out.println("Slot Machines - Costs 5 coins, Win up to 250 coins!");
-        System.out.println("Placeholder");
-        System.out.println("Placeholder");
-        System.out.println("You have " + coins + " coins.");
-        
-        System.out.println("Select an option");
-        String choice = userInput.nextLine();  
-        
-        if (choice.toLowerCase().equals("slots") || choice.equals("1"))
+        while (failsafe == false)
         {
-            if (coins >= 2)
-    	    {
-    	        coins -= 2;
-    	        
-    	        Slots slotGame = new Slots();
-    		    slotGame.Play();
-    
+            System.out.println("Slot Machines - Costs 5 coins, Win up to 250 coins!");
+            System.out.println("Placeholder");
+            System.out.println("Placeholder");
+            System.out.println("You have " + coins + " coins.");
+            
+            System.out.println("Select an option.");
+            String choice = userInput.nextLine();  
+            
+            if (choice.toLowerCase().contains("sl") || choice.toLowerCase().contains("ot") || choice.equals("1"))
+            {
+                if (coins >= 2)
+                {
+                    coins -= 2;
+                    failsafe = true;
+                    Slots slotGame = new Slots();
+                    slotGame.Play();
+        
+                }
+        
+                else if (coins < 2)
+                {
+                    System.out.println("Sorry! You are too POOR! Come back when you're a little... mmmmmmmm... RICHER!");
+                }
             }
-    
-    	    else if (coins < 2)
-    	    {
-    	        System.out.println("Sorry! You are too POOR! Come back when you're a little... mmmmmmmm... RICHER!");
-    	    }
+            else
+            {
+                System.out.println("That's not a valid answer.");
+
+            }
         }
         
     }
--- Slots.java
+++ Slots.java
@@ -5,16 +5,41 @@ public class Slots
     
     public void Play()
     {
-        int terra = 0;
-        while (terra < 50)
+        int auto = 0;
+        int stop = 0;
+        while (stop < 3)
         {
-            for (int i = 0; i < 3; i++)
+
+            if (auto >= 20)
+            {
+                stop++;
+                auto = 0;
+            }
+
+
+            for (int i = stop; i < 3; i++)
             {
                 slots[i] = slotValue[(int) (Math.random() * 3)];
-                System.out.print(slots[0] + slots[1] + slots[2] + "\r");
+                System.out.print("\r" + slots[0] + slots[1] + slots[2]);
+
+                try{
+
+                Thread.sleep(50);
+                }
+                catch (Exception e)
+                {
+                    System.out.println("An error has occured, please try again");
+
+                }
             }
-            terra++;
-        
+            auto++;
+        }
+
+
+        if (slots[0].equals(slots[1]) && slots[1].equals(slots[2]));
+        {
+            System.out.print("\n"); //\u001B[42m
+            System.out.println("You got meony!!");
         }
         
     }
